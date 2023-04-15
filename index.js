10b603ccafce6cde2af3803573a096939b57b283
Red5Gaming <nicompf05@gmail.com>
2023-04-15 16:35:21 +0200

    Did things, forgot what lol

--- index.js
+++ index.js
@@ -9,12 +9,30 @@ const postgres = require("postgres")
 
 const configs = require('./config.json')
 
-app.get('/tournament_by_name', async (_req, res) => {
+// @PARAMS: name
+// Returns a tournament by name
+app.get('/tournament_by_name', async (req, res) => {
     const sql = postgres(configs.connection)
 
-    let name = _req.body.name
+    let name = req.body.name
+
+    if (!name) {
+        res.status(400).send({
+            message: 'Missing required parameter `name`',
+            status: 400
+        })
+        return
+    }
 
     let data = await sql`SELECT * FROM tournaments WHERE name = ${name}`
+
+    if (data.length === 0) {
+        res.status(404).send({
+            message: 'Tournament not found',
+            status: 404
+        })
+        return
+    }
 
     res.send({
         message: data,
@@ -24,6 +42,8 @@ app.get('/tournament_by_name', async (_req, res) => {
 })
 
 
+// @PARAMS: amount
+// Returns the last x tournaments, starting with the newest
 app.get('/last_x_tournament', async (req, res) => {
     const sql = postgres(configs.connection)
 
@@ -52,7 +72,9 @@ app.get('/last_x_tournament', async (req, res) => {
 })
 
 
-
+// @PARAMS: name, placements, date, link, hostlink, host, auth
+// Adds a tournament to the database, placements must be an array of strings
+// Auth is the auth token from the config file
 app.post('/add-tournament', async (req, res) => {
     const sql = postgres(configs.connection)
 
@@ -90,27 +112,41 @@ app.post('/add-tournament', async (req, res) => {
 })
 
 
-
+// @PARAMS: name, placements, date, link, hostlink, host, auth
+// Edits a tournament in the database, placements must be an array of strings
+// Auth is the auth token from the config file
 app.put('/edit-tournament', async (req, res) => {
-    const sql = postgres(configs.connection)
+    const sql = postgres(configs.connection);
 
-    let data = req.body
-    let auth = data.auth
+    let data = req.body;
+    let auth = data.auth;
 
     if (auth === configs.auth) {
         const { name, placements, date, link, hostlink, host, auth } = req.body;
 
+        // Fetch existing data from the database
+        let existingData = await sql`SELECT * FROM tournaments WHERE name = ${name}`;
 
-        let valid = placements.isArray() && placements.every(x => typeof x === 'string')
+        if (existingData.length === 0) {
+            // If no data was found, send a 404 Not Found response
+            res.send({
+                message: 'Tournament not found',
+                status: 404
+            });
+            return;
+        }
+
+        // Check if the placements property is an array and all its elements are strings
+        let valid = Array.isArray(existingData[0].placements) && existingData[0].placements.every(x => typeof x === 'string');
 
         if (!valid) {
+            // If placements is not a valid array, send a 400 Bad Request response
             res.send({
                 message: 'Invalid placements',
                 status: 400
-            })
+            });
         } else {
-
-
+            // Otherwise, update the tournament in the database
             try {
                 await sql`UPDATE tournaments SET placements = ${placements}, date = ${date}, link = ${link}, hostlink = ${hostlink}, host = ${host} WHERE name = ${name}`;
                 res.send({
@@ -124,19 +160,17 @@ app.put('/edit-tournament', async (req, res) => {
                     status: 500
                 });
             }
-
-
         }
-
     } else {
+        // If the authentication token is invalid, send a 401 Unauthorized response
         res.send({
             message: 'Unauthorized',
             status: 401
-        })
+        });
     }
+});
 
 
-})
 
 
 app.listen(PORT, () => {
